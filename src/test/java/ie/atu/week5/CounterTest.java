6d9dc111825996ca5afd7170643c0c3b151eae22
CianODonnellGIT <G00358872@atu.ie>
2022-10-19 10:24:57 +0100

    Adding Calculator and counter Files

--- src/test/java/ie/atu/week5/CounterTest.java
+++ src/test/java/ie/atu/week5/CounterTest.java
@@ -2,23 +2,13 @@ package ie.atu.week5;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
-
+class CounterTest {
+    Counter mycount;
     @BeforeEach
     void setUp() {
-    }
-
-    @Test
-    void  testAdd()
-    {
-        int a = 2;
-        int b = 2;
-        int total = a + b;
-        System.out.println("the total is" + total);
     }
 
     @AfterEach
